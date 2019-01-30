46addf9375883d72d249e53d6f97c3c15305ea72
Solomon Bloch <solomonbloch@gmail.com>
2019-01-30 17:29:13 -0500

    wrote with-token macro, which makes sure that you have a token

--- tokens.lisp
+++ tokens.lisp
@@ -1,16 +1,29 @@
-(in-package :lispotify)
-
-(defvar *client-id* "b0e5640832dd40e69d94009517638de4")
-(defvar *client-secret* "d80c8c477d0540e1b5f3c89de32692ad")
+(in-package #:lispotify)
 
 (defvar *authorization*
   (let ((base-64-encoded (cl-base64:string-to-base64-string
                           (concatenate 'string *client-id* ":" *client-secret*))))
     (concatenate 'string "Basic " base-64-encoded)))
 
-(defvar *token*
-  (parse (dex:post "https://accounts.spotify.com/api/token"
-                   :content '(("grant_type" . "client_credentials"))
-                   :headers `(("Authorization" . ,*authorization*)))))
+(defvar *access-token* "")
 
-(defvar *access-token* (concatenate 'string "Bearer " (val *token* "access_token")))
+(defvar *token*
+  (get-token))
+
+(defun get-token ()
+  (extend-js (parse (dex:post "https://accounts.spotify.com/api/token"
+                              :content '(("grant_type" . "client_credentials"))
+                              :headers `(("Authorization" . ,*authorization*))))
+    ("exp-time" (+ 3600 (get-universal-time)))))
+
+(defun renew-token ()
+  (progn (setf *token*
+               (get-token))
+         (setf *access-token* (concatenate 'string "Bearer " (val *token* "access_token")))))
+
+
+(defmacro with-token (&body body)
+  `(if (> (get-universal-time) (val *token* "exp-time"))
+       (progn (renew-token)
+              ,@body)
+       ,@body))
