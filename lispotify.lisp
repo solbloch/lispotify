65e0014d2e7c700cca90cce49516555b4794c2f3
Solomon Bloch <solomonbloch@gmail.com>
2019-01-31 13:25:10 -0500

    exporting and url-encoding

--- lispotify.lisp
+++ lispotify.lisp
@@ -1,13 +1,12 @@
 (in-package #:lispotify)
 
-(defun cleanup-search (x)
-  (regex-replace-all " " x "%20"))
+(export '(search-track))
 
 (defun search-track (name)
   (with-token
     (let* ((uri (concatenate 'string
                              "https://api.spotify.com/v1/search?q="
-                             (cleanup-search name)
+                             (quri:url-encode name)
                              "&type=track"))
            (raw-results (dex:get uri
                                  :headers `(("Authorization" . ,*access-token*)))))
