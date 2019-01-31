ebd00453c60b96daf0135f2f029b1f44de6a578f
Solomon Bloch <solomonbloch@gmail.com>
2019-01-31 13:25:22 -0500

    exporting and rearranging

--- tokens.lisp
+++ tokens.lisp
@@ -1,14 +1,9 @@
 (in-package #:lispotify)
 
-(defvar *authorization*
-  (let ((base-64-encoded (cl-base64:string-to-base64-string
-                          (concatenate 'string *client-id* ":" *client-secret*))))
-    (concatenate 'string "Basic " base-64-encoded)))
-
-(defvar *access-token* "")
-
-(defvar *token*
-  (get-token))
+(export '(get-token
+          renew-token
+          *token*
+          *access-token*))
 
 (defun get-token ()
   (extend-js (parse (dex:post "https://accounts.spotify.com/api/token"
@@ -16,11 +11,20 @@
                               :headers `(("Authorization" . ,*authorization*))))
     ("exp-time" (+ 3600 (get-universal-time)))))
 
+(defvar *authorization*
+  (let ((base-64-encoded (cl-base64:string-to-base64-string
+                          (concatenate 'string *client-id* ":" *client-secret*))))
+    (concatenate 'string "Basic " base-64-encoded)))
+
+(defvar *token*
+  (get-token))
+
+(defvar *access-token* (concatenate 'string "Bearer " (val *token* "access_token")))
+
 (defun renew-token ()
   (progn (setf *token*
                (get-token))
          (setf *access-token* (concatenate 'string "Bearer " (val *token* "access_token")))))
-
 
 (defmacro with-token (&body body)
   `(if (> (get-universal-time) (val *token* "exp-time"))
